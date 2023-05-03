6f3fee7465cccab35b1be01f60b5329459432dd0
nh1531 <kdwkmj9677@naver.com>
2023-05-03 16:26:21 +0900

    RouterMain 작성

---
+++ FE/src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+
+const LoginPage = () => {
+    return(
+        <>
+            <h1>LoginPage</h1>
+        </>
+    );
+}
+export default LoginPage;
