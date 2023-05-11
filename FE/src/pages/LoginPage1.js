257a0788290dfa6e440cfa8fb3d99b3bf36ecb71
nh1531 <kdwkmj9677@naver.com>
2023-05-11 17:58:48 +0900

    로그인 수정
    - LoginPage 에러 메시지 수정
    - RegisterPage 수정중

--- FE/src/pages/LoginPage1.js
+++ FE/src/pages/LoginPage1.js
@@ -10,25 +10,37 @@ const LoginPage = ({ setIsLogged }) => {
     const [showErrorMessage, setShowErrorMessage] = useState(false);
     const navigate = useNavigate();
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
+    const errorMessage = {
+        color: 'red',
+        textAlign : 'center',
+        fontSize : '0.875rem',
+        marginTop : '1rem',
+    }
 
-        try {
-            const response = await axios.post('/api/Login', { id, password });
-            setShowErrorMessage(false);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const response = await axios.post('/api/Login', {
+                id, 
+                password,
+            });
             console.log(response.data); // 서버에서 반환한 데이터 출력
+            alert('로그인 되었습니다.');
             setIsLogged(true); // 로그인 상태 변경
             localStorage.setItem('isLogged', 'true'); // 로컬 스토리지에 로그인 상태 저장
-            navigate('/'); // 로그인 완료 후 메인 페이지로 이동
-          } catch (error) {
-            setShowErrorMessage(true);
+            // 로그인 완료 후 메인 페이지로 이동
+            navigate('/');
+        } 
+        catch (error) {
             console.log(error);
+            // alert('ID 혹은 비밀번호가 일치하지 않습니다.');
+            
         }
     };
 
     return(
         <div className="logindiv1">
-            <form className="loginform1" onSubmit={handleLogin}>
+            <form className="loginform1" onSubmit={handleSubmit}>
                 <div>
                     < input type="text" id="id" value={id} placeholder="아이디"
                         onChange={(e) => setId(e.target.value)} required />
@@ -37,11 +49,9 @@ const LoginPage = ({ setIsLogged }) => {
                     < input type="password" id="password" value={password} placeholder="비밀번호"
                         onChange={(e) => setPassword(e.target.value)} required />
                 </div>
-                <button onClick={handleLogin}>로그인</button>
-            {showErrorMessage && (
-                <div className="logindiverror">ID 혹은 비밀번호가 일치하지 않습니다.</div>
-            )}
+                <button type="submit">로그인</button>
             </form>
+            <div style={errorMessage}>ID 혹은 비밀번호가 일치하지 않습니다.</div>
             <Link to="/register">회원가입</Link>
         </div>
     );
--- FE/src/pages/RegisterPage1.js
+++ FE/src/pages/RegisterPage1.js
@@ -4,29 +4,15 @@ import axios from 'axios';
 import '../styles/Register.css';
 
 const RegisterPage = () => {
-  // 아이디, 이메일, 비밀번호, 비밀번호 확인
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
   const navigate = useNavigate();
-  
-  // 오류메시지 상태저장
-  const [idMessage, setIdMessage] = useState('');
-  const [passwordMessage, setPasswordMessage] = useState('');
-  const [confirmPasswordMessage, setConfirmPasswordMessage] = useState('');
-  const [userNameMessage, setUserNameMessage] = useState('');
-  const [emailMessage, setEmailMessage] = useState(''); 
 
-  // 유효성 검사
-  const [isId, setIsId] = useState('');
-  const [isPassword, setIsPassword] = useState('');
-  const [isConfirmPassword, setIsConfirmPassword] = useState('');
-  const [isUserName, setIsUserName] = useState('');
-  const [isEmail, setIsEmail] = useState('');
-
-  const handleRegister = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
       alert('비밀번호가 일치하지 않습니다.');
@@ -34,10 +20,10 @@ const RegisterPage = () => {
     } 
     try{
       const response = await axios.post('/addMember', {
-        id : id,
-        password :password,
-        username : userName,
-        email : email,
+        id,
+        password,
+        userName,
+        email,
       });
       console.log(response.data); // 서버에서 반환한 데이터 출력
       alert('회원가입이 완료되었습니다.');
@@ -51,29 +37,9 @@ const RegisterPage = () => {
     }
   };
 
-  // 이름
-  const onChangeName = async(e) => {
-    setUserName(e.target.value);
-    if (e.target.value.length < 2 || e.target.value.length > 5){
-      setUserNameMessage('2글자 이상 5글자 미만으로 입력해주세요.');
-      setIsUserName(false);
-    } 
-  }
-  
-  /*
-  // 이메일
-  const onChangeEmail = (e) => {
-    const emailRegex =
-      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
-    const emailCurrent = e.target.value
-    setEmail(emailCurrent)
-
-  }
-  */
-
   return (
     <div className="registerdiv1">
-      <form className="registerform1" onSubmit={handleRegister}>
+      <form className="registerform1" onSubmit={handleSubmit}>
         <label>아이디</label>
         <input type="text" id="id" value={id} 
           onChange={(e) => setId(e.target.value)} required/>
@@ -88,8 +54,7 @@ const RegisterPage = () => {
           onChange={(e) => setUserName(e.target.value)} />
         <label>이메일</label>
         <input type="email" id="email" value={email} 
-          onChange={onChangeName} />
-        {email.length > 0 && <span className={`message ${isEmail ? 'success' : 'error'}`}>{emailMessage}</span>}
+          onChange={(e) => setEmail(e.target.value)} required/>
         <br />
         <button type="submit">회원가입</button>
       </form>
