a4af53058c56c33daac9a438e913fff354e2b46f
hwk44 <peos0n@kakao.com>
2023-05-17 12:02:13 +0900

    0517

--- DA/app.py
+++ DA/app.py
@@ -7,6 +7,20 @@ import joblib
 
 app = Flask(__name__)
 
+# def encoding(arr):
+#     #  일반 list 인코딩 후 모델에 넣을수 있도록 np.array로 형태변환
+#     encoded_data = []
+#     for i in range(len(arr)):
+#         label = list(label_encoders.keys())[i]  # 라벨링할 열의 키
+#         encoder = label_encoders[label]  # 해당 열의 LabelEncoder 객체
+#         encoded_value = encoder.transform([arr[i]])[0]  # 데이터 라벨링
+#         encoded_data.append(encoded_value)
+#
+#     # 형태변환
+#     encoded_data = np.array(encoded_data)
+#     encoded_data = encoded_data.reshape((1, 4))
+#     return encoded_data
+
 # 메인 페이지 home.html
 @app.route('/')
 def main():
@@ -21,6 +35,19 @@ def main():
 # def get_params(params):
 #     return jsonify({"params" : params})
 
+# def encoding(arr):
+#     #  일반 list 인코딩 후 모델에 넣을수 있도록 np.array로 형태변환
+#     encoded_data = []
+#     for i in range(len(arr)):
+#         label = list(label_encoders.keys())[i]  # 라벨링할 열의 키
+#         encoder = label_encoders[label]  # 해당 열의 LabelEncoder 객체
+#         encoded_value = encoder.transform([arr[i]])[0]  # 데이터 라벨링
+#         encoded_data.append(encoded_value)
+#
+#     # 형태변환
+#     encoded_data = np.array(encoded_data)
+#     encoded_data = encoded_data.reshape((1, 4))
+#     return encoded_data
 
 # predict 페이지에서는 POST method로 값 넘기기
 @app.route('/predict', methods=['POST'])
@@ -29,73 +56,37 @@ def home():
     data2 = request.form['b']
     data3 = request.form['c']
     data4 = request.form['d']
-    arr = np.array([[data1, data2, data3, data4]])
+    # arr = np.array([[data1, data2, data3, data4]])
+    arr = [data1, data2, data3, data4]
+    model = joblib.load(open('./model/model_softmax_0517.pkl', 'rb'))
+    label_encoders = joblib.load(open('./model/model_label_encoders_0517.pkl', 'rb'))
+
+    def encoding(arr):
+        #  일반 list 인코딩 후 모델에 넣을수 있도록 np.array로 형태변환
+        encoded_data = []
+        for i in range(len(arr)):
+            label = list(label_encoders.keys())[i]  # 라벨링할 열의 키
+            encoder = label_encoders[label]  # 해당 열의 LabelEncoder 객체
+            encoded_value = encoder.transform([arr[i]])[0]  # 데이터 라벨링
+            encoded_data.append(encoded_value)
+
+        # 형태변환
+        encoded_data = np.array(encoded_data)
+        encoded_data = encoded_data.reshape((1, 4))
+        return encoded_data
+
+    arr = encoding(arr)
     pred = model.predict(arr)
     # pred = model.predict(encoded_data)
     pred = label_encoders["key2"].inverse_transform(pred)
     print(pred)
-    return render_template('after.html', data=pred)
+    return render_template('after.html', data=pred[0])
 
-@app.route('/predict', methods=['GET'])
-def predict():
-    return render_template('after.html')
+# @app.route('/predict', methods=['GET'])
+# def predict():
+#     return render_template('after.html')
 
 if __name__ == '__main__':
     # model, label encoders 딕셔너리 로딩
-    model = joblib.load(open('./model/model_softmax.pkl', 'rb'))
-    label_encoders = joblib.load(open('./model/model_label_encoders_0517.pkl', 'rb'))
     # Flask 스타트
     app.run(host='0.0.0.0', port=5000, debug=True)
-
-# @app.route('/api/predict', methods=['POST','GET'])
-# def predict():
-#     # params = json.loads(request.json(), encoding='utf-8')
-#     # params = request.get_json()
-    
-#     char1 = request.form.get('char1')
-#     num = request.form.get('num')
-#     params = {}
-#     params['char1'] = char1
-#     params['num'] = num
-#     params = json.dumps(params)
-#     return render_template('app.html', num=num)
-
-# @app.route('/calculate', methods=['POST', 'GET'])
-# def calculate(num=None):
-#     ## 어떤 http method를 이용해서 전달받았는지를 아는 것이 필요함
-#     ## 아래에서 보는 바와 같이 어떤 방식으로 넘어왔느냐에 따라서 읽어들이는 방식이 달라짐
-#     if request.method == 'POST':
-#         params = {}
-#         params['num'] = request.form.get['num']
-#         params['char1']  = request.form.get['char1']
-#         return jsonify(json.dumps(params))
-#         pass
-#     elif request.method == 'GET':
-#         ## 넘겨받은 숫자
-#         temp = request.args.get('num')
-#         temp = int(temp)
-#         ## 넘겨받은 문자
-#         temp1 = request.args.get('char1')
-#         ## 넘겨받은 값을 원래 페이지로 리다이렉트
-#         return render_template('app.html', num=temp, char1=temp1)
-    
-    
-    ## else 로 하지 않은 것은 POST, GET 이외에 다른 method로 넘어왔을 때를 구분하기 위함
-    # return jsonify(params)
-    # params_str = ''
-    # for key in params.keys():
-        # params_str += 'key: {}, value: {}<br>'.format(key, params[key])
-    # return params_str
-    # content = request.json
-    # print(content)
-    # Sex = content['Sex']
-    # Age = content['Age']
-    # # return jsonify({'prediction': str(prediction)})
-    # return jsonify(content)
-    
-# @app.route('/create', methods=['POST'])
-# def create():
-#     print(request.is_json)
-#     params = request.get_json()
-#     print(params['user_id'])
-#     return 'ok'
--- DA/test.py
+++ DA/test.py
@@ -1,11 +1,35 @@
 import pandas as pd
 import numpy as np
+import joblib
 from sklearn.model_selection import train_test_split
-df = pd.read_csv('./data/raw_postpro.csv', encoding='cp949')
-df1 = df[['Machinery', 'Assembly' , "Part No.1","청구품목", 'key2']]
+# df = pd.read_csv('./data/raw_postpro.csv', encoding='cp949')
+# df1 = df[['Machinery', 'Assembly' , "Part No.1","청구품목", 'key2']]
 
-print(df1)
+# print(df1)
 # new_data = np.array(['SKIFF BOAT', 'IMPLANTATION INSTRUMENTS', '14.9795', 'EXHAUST GAS TEMPERATURE INDICATOR 0 TO 700℃'])
 # col_name = ['Machinery', 'Assembly' , "Part No.1","청구품목", 'key2']
 # for a,b in zip(new_data, col_name):
     # a = label_encoders[b].fit(a)
+
+model = joblib.load(open('./model/model_softmax_0517.pkl', 'rb'))
+label_encoders = joblib.load(open('./model/model_label_encoders_0517.pkl', 'rb'))
+def encoding(arr):
+    #  일반 list 인코딩 후 모델에 넣을수 있도록 np.array로 형태변환
+    encoded_data = []
+    for i in range(len(arr)):
+        label = list(label_encoders.keys())[i]  # 라벨링할 열의 키
+        encoder = label_encoders[label]  # 해당 열의 LabelEncoder 객체
+        encoded_value = encoder.transform([arr[i]])[0]  # 데이터 라벨링
+        encoded_data.append(encoded_value)
+
+    # 형태변환
+    encoded_data = np.array(encoded_data)
+    encoded_data = encoded_data.reshape((1, 4))
+    return encoded_data
+
+arr = ['SKIFF BOAT', 'IMPLANTATION INSTRUMENTS', '14.9795', 'EXHAUST GAS TEMPERATURE INDICATOR 0 TO 700℃']
+arr = encoding(arr)
+pred = model.predict(arr)
+pred = label_encoders["key2"].inverse_transform(pred)
+print(pred)
+# print(model.predict(arr))
