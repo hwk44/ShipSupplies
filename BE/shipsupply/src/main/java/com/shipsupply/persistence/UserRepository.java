7b1af7be8cdba321f59de3cab14f257b583a7d90
KEN ROE <73014470+KHEN-ROE@users.noreply.github.com>
2023-05-17 17:47:34 +0900

    Add files via upload

---
+++ BE/shipsupply/src/main/java/com/shipsupply/persistence/UserRepository.java
@@ -0,0 +1,11 @@
+package com.shipsupply.persistence;
+
+import com.shipsupply.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
