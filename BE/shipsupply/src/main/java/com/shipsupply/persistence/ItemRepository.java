4e12c813fe7b5e78876bf3ce0db7451c4d2e6bbe
KEN ROE <73014470+KHEN-ROE@users.noreply.github.com>
2023-05-21 00:50:33 +0900

    Delete BE/shipsupply/src directory

--- BE/shipsupply/src/main/java/com/shipsupply/persistence/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shipsupply.persistence;
-
-import com.shipsupply.domain.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-
-}
