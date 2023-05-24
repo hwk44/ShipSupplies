08f07121d897eae35e60eb7fb002e3c9d6fd8e47
nh1531 <kdwkmj9677@naver.com>
2023-05-24 12:47:50 +0900

    Update PredictionPage.js
    - headers token 추가

--- FE/src/pages/PredictionPage.js
+++ FE/src/pages/PredictionPage.js
@@ -13,12 +13,21 @@ const PredictionPage = () => {
     const handlePred = async (e) => {
         e.preventDefault();
 
-        try {
-            const response = await axios.post("/api/item/predict/classify", { machinery, assembly, partno1, item });
-            console.log(response.data); // 서버에서 반환한 데이터 출력
-        } catch (error) {
-            console.log(error);
-        }
+        const token = localStorage.getItem('jwt');
+        axios.post("/api/item/predict/classify", { machinery, assembly, partno1, item }, {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((response) => {
+                console.log(response.data); // 서버에서 반환한 데이터 출력
+                // 성공적인 응답에 대한 추가 처리
+            })
+            .catch((error) => {
+                console.log(error);
+                // 요청 실패 시 에러 처리
+            });
     };
 
 
@@ -27,16 +36,16 @@ const PredictionPage = () => {
             <h3>Classification Prediction</h3>
             <form className="predictionform" onSubmit={handlePred}>
                 <div>
-                    <TextField id="outlined-basic" label="Machinery" variant="outlined" onChange={(e) => setMachinery(e.target.value)} />
+                    <TextField id="outlined-basic" label="Machinery" variant="outlined" value={machinery} onChange={(e) => setMachinery(e.target.value)} />
                 </div>
                 <div>
-                    <TextField id="outlined-basic" label="Assembly" variant="outlined" onChange={(e) => setAssembly(e.target.value)} required />
+                    <TextField id="outlined-basic" label="Assembly" variant="outlined" value={assembly} onChange={(e) => setAssembly(e.target.value)} required />
                 </div>
                 <div>
-                    <TextField id="outlined-basic" label="Part No.1" variant="outlined" onChange={(e) => setPartno1(e.target.value)} required/>
+                    <TextField id="outlined-basic" label="Part No.1" variant="outlined" value={partno1} onChange={(e) => setPartno1(e.target.value)} required />
                 </div>
                 <div>
-                    <TextField id="outlined-basic" label="Item" variant="outlined" onChange={(e) => setItem(e.target.value)} required/>
+                    <TextField id="outlined-basic" label="Item" variant="outlined" value={item} onChange={(e) => setItem(e.target.value)} required />
                 </div>
                 <Button variant="contained" size="medium" type="submit">
                     분류 예측
