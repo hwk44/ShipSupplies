f84387182d9113cf7f6d6a5a8bedbac793134a85
hwk44 <peos0n@kakao.com>
2023-05-30 14:42:41 +0900

    flask predict/regression api

--- DA/app.py
+++ DA/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify, render_template, request, url_for
-import pickle
 import numpy as np
-import json
 import joblib
+import joblib
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 app = Flask(__name__)
 
@@ -51,6 +52,41 @@ def home():
     # 예측 값 디코딩 
     pred = label_encoders["key2"].inverse_transform(pred)
     return jsonify({"datas": datas, "pred": pred[0]})
+
+@app.route('/api/item/predict/regression', methods=['POST'])
+def home():
+    # loading pkl file
+    # scaler
+    scaler = joblib.load(open('./model/scaler.pkl', 'rb'))
+    # tokenizer
+    tokenizer = joblib.load(open('./model/tokenizer.pkl', 'rb'))
+    # 모델 불러오기
+    model = tf.keras.models.load_model("./model/model_regression.h5")
+    # react 에서 form 으로 4개 값을 json 으로 보냄.
+    data = request.get_json()
+
+    # 공백 제거
+    data1 = data['a'].strip()
+    data2 = data['b'].strip()
+    data3 = data['c'].strip()
+    data4 = data['d'].strip()
+    data5 = data['e'].strip()
+    data6 = data['f'].strip()
+    datas = [data1, data2, data3, data4, data5, data6]
+
+    datas = " ".join(datas)
+
+    combined_sequence = tokenizer.texts_to_sequences([datas])
+
+    # 시퀀스 패딩
+    combined_padded_sequence = pad_sequences(combined_sequence, maxlen=100)
+
+    # 예측
+    pred = model.predict(combined_padded_sequence)
+    # 원래 스케일로 되돌리기
+    pred = scaler.inverse_transform(pred)
+
+    return jsonify({"datas": datas, "pred": pred[0][0]})
 
 
 # 플라스크 5000 번 에서 실행하는 코드. form 으로 4개 입력값을 받은 후에 결과값 확인
