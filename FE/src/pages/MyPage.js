4f099f1f5654d2b5f945bfa15c12883a8eb91bc3
n10531 <122756254+nh1531@users.noreply.github.com>
2023-05-31 12:38:34 +0900

    MyPage 사이드바 작성
    - 고정 안되는 부분 수정 필요

--- FE/src/pages/MyPage.js
+++ FE/src/pages/MyPage.js
@@ -5,6 +5,7 @@ import '../styles/MyPage.css';
 import UserDelete from '../components/user/UserDelete';
 import UserPwdUpdate from '../components/user/UserPwdUpdate';
 import UserUpdate from '../components/user/UserUpdate';
+import Navbar from "../components/mypage/Navbar";
 
 
 
@@ -12,14 +13,23 @@ import UserUpdate from '../components/user/UserUpdate';
 const MyPage = () => {
     return (
         <>
-            <Sidebar>
+            {/* <Sidebar>
                 <Routes>
                     <Route exact path="/" element={<UserUpdate />} />
                     <Route exact path="/userupdate" element={<UserUpdate />} />
                     <Route path="/userpwdupdate" element={<UserPwdUpdate />} />
                     <Route path="/userdelete" element={<UserDelete />} />
                 </Routes>
-            </Sidebar>
+            </Sidebar> */}
+            {/* <Router> */}
+                <Navbar />
+                <Routes>
+                    {/* <Route exact path="/" element={<UserUpdate />} /> */}
+                    <Route exact path="/userupdate" component={UserUpdate} />
+                    <Route path="/userpwdupdate" component={UserPwdUpdate} />
+                    <Route path="/userdelete" component={UserDelete} />
+                </Routes>
+            {/* </Router> */}
         </>
     );
 }
