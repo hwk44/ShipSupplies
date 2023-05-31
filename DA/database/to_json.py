ec14e88b7caabaf9fffd7c88465aa16b2c96c6a6
hwk44 <peos0n@kakao.com>
2023-05-31 17:49:56 +0900

    검색 드롭박스

--- DA/database/to_json.py
+++ DA/database/to_json.py
@@ -1,46 +1,99 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('../data/raw_postpro.csv')
+df = pd.read_csv('../data/raw_postpro.csv', encoding='cp949')
 
-y = df["key2"]
-sorted_df_y = y.sort_values( ascending=True)
-y = sorted_df_y.unique()
-# ndarray를 리스트로 변환
-y = y.tolist()
-print(y)
-
-import json
-# JSON 파일 경로
-file_path = './category.json'
-
-# 리스트를 JSON 형식으로 직렬화하여 파일에 저장
-with open(file_path, 'w') as file:
-    json.dump(y, file)
-
-
-
-import json
-
-# 좌우 공백, " , ' 제거
 df["청구품목"] = df["청구품목"].str.strip()
 df["청구품목"] = df["청구품목"].str.replace('"', '')
 df["청구품목"] = df["청구품목"].str.replace("'", "")
 
 
-# 중복 항목 제거
-df_unique = df.drop_duplicates(subset=["key2", "청구품목"])
+# 각 컬럼의 중복 제거된 값들을 모은 딕셔너리 생성
+unique_dict = {}
+for col in ['발주처', '청구품목', 'key2']:
+    unique_dict[col] = df[col].drop_duplicates().tolist()
 
-# 아이템 그룹화
-grouped_data = df_unique.groupby("key2")["청구품목"].apply(list).reset_index()
-grouped_data_dict = dict(zip(grouped_data["key2"], grouped_data["청구품목"]))
-# print(grouped_data_dict)
-sum = 0
-for category, items in grouped_data_dict.items():
-    print(f"{category}: {len(items)}")
-    sum += len(items)
-print(sum)
+(unique_dict)
 
-# JSON 파일로 저장
-with open("items.json", "w") as file:
-    json.dump(grouped_data_dict, file, indent=4)
+for k in unique_dict.keys() :
+    # print(unique_dict[k])
+    print(k)
+
+# import json
+# # # JSON 파일 경로
+# file_path = './datas.json'
+# # 리스트를 JSON 형식으로 직렬화하여 파일에 저장
+# with open(file_path, 'w',  encoding='utf-8') as file:
+#      json.dump(unique_dict, file, ensure_ascii=False, indent=4)
+
+
+
+# y = df["key2"]
+# sorted_df_y = y.sort_values( ascending=True)
+# y = sorted_df_y.unique()
+# # ndarray를 리스트로 변환
+# y = y.tolist()
+# print(y)
+#
+# import json
+# # JSON 파일 경로
+# file_path = './category.json'
+#
+# # 리스트를 JSON 형식으로 직렬화하여 파일에 저장
+# with open(file_path, 'w') as file:
+#     json.dump(y, file)
+
+
+
+import json
+
+# 청구품목만 js 파일로 저장
+# 좌우 공백, " , ' 제거
+# df["청구품목"] = df["청구품목"].str.strip()
+# df["청구품목"] = df["청구품목"].str.replace('"', '')
+# df["청구품목"] = df["청구품목"].str.replace("'", "")
+# df_unique = df.drop_duplicates(subset=["청구품목"])["청구품목"].tolist()
+# print(len(df_unique))
+
+# import json
+# JSON 파일 경로
+# file_path = './itemnames.json'
+
+# 리스트를 JSON 형식으로 직렬화하여 파일에 저장
+# with open(file_path, 'w',  encoding='utf-8') as file:
+#     json.dump(df_unique, file, ensure_ascii=False, indent=4)
+
+
+
+# 발주처 json 파일 저장
+# df["발주처"] = df["발주처"].str.strip()
+# df_unique = df.drop_duplicates(subset=["발주처"])["발주처"].tolist()
+# print(df_unique)
+#
+# import json
+# JSON 파일 경로
+# file_path = './company.json'
+
+# 리스트를 JSON 형식으로 직렬화하여 파일에 저장
+# with open(file_path, 'w', encoding='utf-8') as file:
+#     json.dump(df_unique, file, ensure_ascii=False, indent=4)
+
+# # 중복 항목 제거
+# df_unique = df.drop_duplicates(subset=["key2", "청구품목"])
+#
+# # 아이템 그룹화
+# grouped_data = df_unique.groupby("key2")["청구품목"].apply(list).reset_index()
+# grouped_data_dict = dict(zip(grouped_data["key2"], grouped_data["청구품목"]))
+# # print(grouped_data_dict)
+# sum = 0
+# for category, items in grouped_data_dict.items():
+#     print(f"{category}: {len(items)}")
+#     sum += len(items)
+# print(sum)
+#
+# # JSON 파일로 저장
+# with open("items.json", "w") as file:
+#     json.dump(grouped_data_dict, file, indent=4)
+
+
+
--- FE/src/pages/SearchPage.js
+++ FE/src/pages/SearchPage.js
@@ -1,77 +1,136 @@
 import items from '../db/items.json';
-import Dropdown from "./Dropdown";
+import data from '../db/datas.json'
+import axios from 'axios';
+import Dropdown from './Dropdown';
 // import { useState, useEffect, useRef } from 'react';
 import React, { useState, useEffect, useRef } from 'react';
 
 
-
-
 const SearchPage = () => {
 
+  // 드롭다운 가시화 변수
   const [dropdownVisibility, setDropdownVisibility] = useState(false);
+ 
+  // 드롭다운에서 선택된 항목 정보 저장하는 변수
+  const [selectedItem, setSelectedItem] = useState(''); // 추가된 코드
+  // 
+  const [key, setKey] = useState(false); //
 
-  const [selectedItem, setSelectedItem] = useState('');
+  const [list1, setList1] = useState([]);
 
+  useEffect(() => {
+    // console.log('선택된 항목:', selectedItem);
+    if (selectedItem.includes("카테고리")){
+      setKey("key2")
+    } else if(selectedItem.includes("청구품목")){
+      setKey("청구품목")
+    } else if (selectedItem.includes("발주처")){
+      setKey("발주처")
+    } 
+    
+  }, [selectedItem]);
+
+  // key 값이 변할때 data를 다르게 불러오도록 하는 useEffect
+  useEffect(() => {
+    // console.log(key)
+    const newList1 = data[key];
+    setList1(newList1);
+    // console.log(list1)
+  }, [key]);
+
+  // useEffect(() => {
+  //   console.log(list1)
+  // }, [list1]);
+
+
+  // 드롭박스 항목 클릭 함수
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+    setDropdownVisibility(false);
+    // console.log(buttonText);
+  }; 
+
+  // 선택된 항목 buttonText
+  const buttonText = selectedItem || '항목선택';
 
   const [selcate, setSelcate] = useState();
   const txtC = useRef();
 
   const cate = Object.keys(items);
-  console.log(cate);
+  // console.log(cate);
 
-  // 맨 처음 한번 실행될 때 useRef에 포커스
-  useEffect(() => {
-    txtC.current.focus();
-  }, []);
 
   const selItem = (i) => {
     setSelcate(i);
     txtC.current.value = i;
+    setCTag([]); // txtC가 선택되면 아래항목 보이지않게 배열 초기화
   }
 
-  // 초기값 빈배열[]
-  const [ctag, setCTag] = useState([]);
-  const showC1 = (e) => {
-    const searchText = txtC.current.value.toLowerCase();
-    const temp = cate.filter((i) => i.toLowerCase().includes(searchText));
-    setCTag(
-      temp.map((i) => (
-        <li onClick={() => selItem(i)}key={i}>{i}</li>
-      ))
-    );
-  };
-  
+    // 초기값 빈배열[]
+    const [ctag, setCTag] = useState([]);
+
+    const showC1 = (e) => {
+      // txtC 값을 소문자로 바꾸고
+      const searchText = txtC.current.value.toLowerCase();
+      // 그 값을 포함하는 list filter로 거름 
+      const temp = list1.filter((i) => i.toLowerCase().includes(searchText));
+      setCTag(
+        temp.map((i) => (
+          <li onClick={() => selItem(i)}key={i}>{i}</li>
+        ))
+      );
+    };
+
+    // Button 클릭시 txtC, key 값을 넘기는 post 요청
+    const handleSubmit = async () => {
+      try {
+        let requestUrl;
+    
+        if (key === "카테고리") {
+          requestUrl = "/api/item/findByCategory";
+        } else if (key === "청구품목") {
+          requestUrl = "/api/item/findByItem";
+        } else if (key === "발주처") {
+          requestUrl = "/api/item/findByCompany";
+        } else {
+          console.error("Invalid key value:", key);
+          return;
+        }
+    
+        const response = await axios.post(requestUrl, {
+          key: key,
+          txtCValue: txtC.current.value,
+        });
+    
+        console.log(response.data);
+      } catch (error) {
+        console.error("POST 요청 에러:", error);
+      }
+    };
 
   return (
     <>
-    <div className="conleft">
-      <div>
-        <input ref={txtC} type="text" name="txt1" onChange={showC1} placeholder="카테고리"/>
-        <input type="text" name="txt2" placeholder="물품" />
-      </div>
-          <ul>{ctag.map((item) => item)}</ul>
-    </div>
-
-    <button onClick={e => setDropdownVisibility(!dropdownVisibility)}>
-      {dropdownVisibility ? '닫기': '항목선택'}
+    <button onClick={() => setDropdownVisibility(!dropdownVisibility)}>
+        {dropdownVisibility ? '닫기' : buttonText}
     </button> 
 
     <div className='app'>
-      <Dropdown visibility={dropdownVisibility}>
+        <Dropdown visibility={dropdownVisibility}>
           <ul>
-              <li>발주처</li>
-              <li>부품명(청구품목)</li> 
-              <li>카테고리(key2)</li>
-              {/* <li>item 4</li> */}
+            <li onClick={() => handleItemClick("발주처")}>발주처</li>
+            <li onClick={() => handleItemClick("부품명(청구품목)")}>부품명(청구품목)</li> 
+            <li onClick={() => handleItemClick("카테고리(key2)")}>카테고리(key2)</li>
           </ul>
-      </Dropdown>
-    </div>
-
-
-
+        </Dropdown>
+        <input ref={txtC} type="text" name="txt1" onChange={showC1} placeholder={key || "항목을 먼저 선택해 주세요"}/>
+        <br /><br />
+        <button onClick={handleSubmit}>제출</button>
+        <div className="conleft">
+          {txtC.current && txtC.current.value.length > 0 ? (
+            <ul>{ctag.slice(0, 30).map((item) => item)}</ul>) : null}
+        </div>
+      </div>
     </>
-   
   );
-
 }
 export default SearchPage;
