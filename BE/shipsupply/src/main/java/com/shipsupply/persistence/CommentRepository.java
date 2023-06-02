dbd8211064be2e880821a060d46b74632090ad61
KEN ROE <73014470+KHEN-ROE@users.noreply.github.com>
2023-06-02 10:00:34 +0900

    최신본 (#51)
    최신본 (#51)

--- BE/shipsupply/src/main/java/com/shipsupply/persistence/CommentRepository.java
+++ BE/shipsupply/src/main/java/com/shipsupply/persistence/CommentRepository.java
@@ -7,6 +7,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByOrderByHitCountDesc();
     List<Comment> findAllByBoardIdOrderByHitCountDesc(Long BoardId);
 }
