0969407b2d716d0f1c67485c2d2ece7a13ffed19
KEN ROE <73014470+KHEN-ROE@users.noreply.github.com>
2023-06-08 10:07:41 +0900

    Update OAuth2AuthenticationSuccessHandler.java

--- BE/shipsupply/src/main/java/com/shipsupply/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ BE/shipsupply/src/main/java/com/shipsupply/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -55,12 +55,6 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         userIdCookie.setDomain("localhost"); // 클라이언트 도메인 설정. 없어도 되는듯
         response.addCookie(userIdCookie);
 
-
-        Cookie userIdCookie = new Cookie("userId", authentication.getName());
-        userIdCookie.setHttpOnly(false);
-        tokenCookie.setPath("/");
-        response.addCookie(userIdCookie);
-
         clearAuthenticationAttributes(request);  //인증 과정에서 저장된 세션을 정리
 
 
