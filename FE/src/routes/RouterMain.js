83d8c4db58e81f0f45237ad9ff90541f83166796
nh1531 <122756254+nh1531@users.noreply.github.com>
2023-06-09 15:00:25 +0900

    SearchPage 검색창 수정 (#62)
    * SearchPage 검색창 수정

--- FE/src/routes/RouterMain.js
+++ FE/src/routes/RouterMain.js
@@ -14,6 +14,7 @@ import PredictionPage from '../pages/PredictionPage';
 import RegressionPage from '../pages/RegressionPage';
 import HelpDesk from '../pages/HelpDesk';
 import PostDetail from '../pages/PostDetail';
+import PastLeadtime from '../components/PastLeadtime';
 
 import { useState, useEffect } from 'react';
 
@@ -45,6 +46,7 @@ const RouterMain = () => {
                 < Route path="/regression" element={<RegressionPage />} />
                 < Route path="/helpdesk" element={<HelpDesk />} />
                 < Route path="/post/:id" element={<PostDetail />} />
+                < Route path="/pastleadtime" element={<PastLeadtime />} />
                 
             </Routes>
         </>
