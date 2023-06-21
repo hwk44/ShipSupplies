446e455227e8544c2324fe850fdad3dc5b9c84c8
KEN <shdnrnjs@gmail.com>
2023-06-21 17:36:04 +0900

    nav 수정

--- FE/src/routes/RouterNav.js
+++ FE/src/routes/RouterNav.js
@@ -5,7 +5,6 @@ import { BsPerson, BsCart3 } from 'react-icons/bs';
 import { LuLogOut } from "react-icons/lu";
 import Dropdown from '../components/Dropdown';
 import axios from 'axios';
-import { useState } from 'react';
 
 const RouterNav = () => {
     const navigate = useNavigate();
@@ -20,13 +19,13 @@ const RouterNav = () => {
 
     const isLoggedIn = !!localStorage.getItem('userId');
     const userId = localStorage.getItem('userId');
-    
+
 
     const handleLogout = async () => {
-        try{
+        try {
             // 서버에 로그아웃 요청
             const response = await axios.post('/api/user/logout', {
-                user: {id: userId}
+                user: { id: userId }
             });
 
             console.log(response)
@@ -40,39 +39,50 @@ const RouterNav = () => {
         }
     };
 
+    const handleLinkClick = (event) => {
+        if(userId == null) {
+            alert('로그인이 필요한 서비스입니다.')
+            event.preventDefault();
+            navigate('/login');
+        }
+    }
+
     return (
         <nav>
             <div className='flex items-center justify-between my-4 mr-5 h-36'>
                 <Logo />
-                <ul className='flex flex-row space-x-4 items-center mr-12'>
+                    <ul className='flex flex-row space-x-4 items-center mr-12'>
+
                         <li>
-                            <a href="/prediction" className="hover:text-blue-700 ">
+                            <a href="/prediction" className="hover:text-blue-700 " onClick={handleLinkClick}>
                                 카테고리 분류
                             </a>
+
                         </li>
-                        
+
+
                         <li>
-                            <a href="/regression" className="hover:text-blue-700">
+                            <a href="/regression" className="hover:text-blue-700" onClick={handleLinkClick}>
                                 리드타임 예측
                             </a>
                         </li>
-                        
+
                         <li>
-                            <a href="/search" className="hover:text-blue-700">
+                            <a href="/search" className="hover:text-blue-700" onClick={handleLinkClick}>
                                 선용품 검색
                             </a>
                         </li>
                         <li>
-                            <a href="/helpdesk" className="hover:text-blue-700">
+                            <a href="/helpdesk" className="hover:text-blue-700" onClick={handleLinkClick}>
                                 고객센터
                             </a>
                         </li>
                         <li>
-                            <a href="/data" className="hover:text-blue-700">
+                            <a href="/data" className="hover:text-blue-700" onClick={handleLinkClick}>
                                 데이터
                             </a>
                         </li>
-                        
+
                         <div className="flex items-center">
                             <Dropdown className="my-1" />
                             <button
@@ -80,24 +90,25 @@ const RouterNav = () => {
                                 className="mx-2.5 text-gray-400 hover:text-black mb-3"
                                 onClick={goCart}
                             >
-                                <BsCart3 size={25}/>
+                                <BsCart3 size={25} />
                             </button>
 
                             {/* 로그인 시 로그아웃 버튼 보이게 */}
                             {isLoggedIn ? (
                                 <button
-                                type="button"
-                                className="mx-2.5 mb-2.5 text-gray-400 hover:text-black flex flex-direction: row; "
-                                onClick={handleLogout}
+                                    type="button"
+                                    className="mx-2.5 mb-2.5 text-gray-400 hover:text-black flex flex-direction: row; "
+                                    onClick={handleLogout}
                                 >
-                                로그아웃
-                                <LuLogOut size={20}/>
+                                    로그아웃
+                                    <LuLogOut size={20} />
                                 </button>
                             ) : (
                                 <div></div>
                             )}
                         </div>
                     </ul>
+               
             </div>
         </nav>
     );
