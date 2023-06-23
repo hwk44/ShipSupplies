eb2c97fbe2bff5aa3de4cb5b4b9cda37e43a250e
nh1531 <kdwkmj9677@naver.com>
2023-06-23 17:56:49 +0900

    HelpDeskPage 수정
    - HelpDeskPage css 수정
    - HelpDeskPage 페이지네이션 작성중

--- FE/src/pages/PostDetailPage.js
+++ FE/src/pages/PostDetailPage.js
@@ -86,7 +86,7 @@ const PostDetail = () => {
                                 </button>
                                 <button onClick={deleteBoard}
                                     className="bg-transparent hover:bg-red-100 text-red-500 py-2 px-4 border border-gray-300 hover:border-red-400 rounded mb-3">
-                                    삭제
+                                    삭제 
                                 </button>
                             </div>
                         ) 
