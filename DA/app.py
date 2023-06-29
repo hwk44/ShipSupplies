97d2bd28c9439f2680dfe9756253e088654762b4
hwk44 <peos0n@kakao.com>
2023-06-29 12:50:18 +0900

    6개 컬럼 분류 모델 완성

--- DA/app.py
+++ DA/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, jsonify, render_template, request, url_for
+from flask import Flask, jsonify, render_template, request
 import numpy as np
 import joblib
-import joblib
 import tensorflow as tf
+import os
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 app = Flask(__name__)
@@ -16,9 +16,20 @@ def main():
 
 @app.route('/api/item/predict/classify', methods=['POST'])
 def home():
+
     # loading pkl file
-    model = joblib.load(open('./model/model_softmax.pkl', 'rb'))
-    label_encoders = joblib.load(open('./model/model_label_encoders.pkl', 'rb'))
+    # encoder = joblib.load(open('labelencoder_0629.pkl', 'rb'))
+    # tokenizer = joblib.load(open('tokenizer_0629.pkl', 'rb'))
+    # model = tf.keras.models.load_model('model_classification_0629.h5')
+
+    encoder = joblib.load(open('./model/labelencoder_0629.pkl', 'rb'))
+
+    # tokenizer
+    tokenizer = joblib.load(open('./model/tokenizer_0629.pkl', 'rb'))
+
+    # 모델 불러오기
+    model = tf.keras.models.load_model("./model/model_classification_0629.h5")
+
 
     # react 에서 form 으로 4개 값을 json 으로 보냄.
     data = request.get_json()
@@ -28,30 +39,24 @@ def home():
     data2 = data['b'].strip()
     data3 = data['c'].strip()
     data4 = data['d'].strip()
-    datas = [data1, data2, data3, data4]
-    arr = [data1, data2, data3, data4]
+    data5 = data['e'].strip()
+    data6 = data['f'].strip()
 
-    def encoding(arr): # 입력 data(arr) 인코딩 함수
-        #  일반 list 인코딩 후 모델에 넣을수 있도록 np.array로 형태변환
-        encoded_data = []
-        for i in range(len(arr)):
-            label = list(label_encoders.keys())[i]  # 라벨링할 열의 키
-            encoder = label_encoders[label]  # 해당 열의 LabelEncoder 객체
-            encoded_value = encoder.transform([arr[i]])[0]  # 데이터 라벨링
-            encoded_data.append(encoded_value)
+    datas = [data1, data2, data3, data4, data5, data6]
+    arr = [data1, data2, data3, data4, data5, data6]
 
-        # 형태변환 모델에 예측 값으로 넘길수 있도록
-        encoded_data = np.array(encoded_data)
-        encoded_data = encoded_data.reshape((1, 4))
-        return encoded_data
+    combined_sequence = tokenizer.texts_to_sequences([" ".join(datas)])
 
-    # arr 인코딩
-    arr = encoding(arr)
-    # 인코딩된 예측
-    pred = model.predict(arr)
-    # 예측 값 디코딩 
-    pred = label_encoders["key2"].inverse_transform(pred)
-    return jsonify({"datas": datas, "pred": pred[0]})
+    max_len = 36
+    combined_padded_sequence = pad_sequences(combined_sequence, maxlen=max_len)
+
+    # 예측
+    y_pred = model.predict(combined_padded_sequence)
+
+    y_pred_result = encoder.inverse_transform(np.argmax(y_pred, axis=1))
+    # 예측 값 디코딩
+    # pred = label_encoders["key2"].inverse_transform(pred)
+    return jsonify({"datas": datas, "pred": y_pred_result[0]})
 
 @app.route('/api/item/predict/regression', methods=['POST'])
 def home1():
@@ -93,39 +98,36 @@ def home1():
 # 플라스크 5000 번 에서 실행하는 코드. form 으로 4개 입력값을 받은 후에 결과값 확인
 @app.route('/item/predict/classify', methods=['POST'])
 def home_flask():
+    print(os.getcwd())
+
     # loading pkl file
-    model = joblib.load(open('./model/model_softmax.pkl', 'rb'))
-    label_encoders = joblib.load(open('./model/model_label_encoders.pkl', 'rb'))
+    encoder = joblib.load(open('labelencoder_0629.pkl', 'rb'))
+    tokenizer = joblib.load(open('tokenizer_0629.pkl', 'rb'))
+    model = tf.keras.models.load_model('model_classification_0629.h5')
 
     # form 입력에서 공백 제거
     data1 = request.form.get('a').strip()
     data2 = request.form.get('b').strip()
     data3 = request.form.get('c').strip()
     data4 = request.form.get('d').strip()
-    datas = [data1, data2, data3, data4]
-    arr = [data1, data2, data3, data4]
+    data5 = request.form.get('e').strip()
+    data6 = request.form.get('f').strip()
 
-    def encoding(arr): # 입력 data(arr) 인코딩 함수
-        #  일반 list 인코딩 후 모델에 넣을수 있도록 np.array로 형태변환
-        encoded_data = []
-        for i in range(len(arr)):
-            label = list(label_encoders.keys())[i]  # 라벨링할 열의 키
-            encoder = label_encoders[label]  # 해당 열의 LabelEncoder 객체
-            encoded_value = encoder.transform([arr[i]])[0]  # 데이터 라벨링
-            encoded_data.append(encoded_value)
+    datas = [data1, data2, data3, data4, data5, data6]
+    # arr = [data1, data2, data3, data4, data5, data6]
 
-        # 형태변환
-        encoded_data = np.array(encoded_data)
-        encoded_data = encoded_data.reshape((1, 4))
-        return encoded_data
+    combined_sequence = tokenizer.texts_to_sequences([" ".join(datas)])
 
-    # arr 인코딩
-    arr = encoding(arr)
-    # 인코딩된 예측
-    pred = model.predict(arr)
+    max_len = 36
+    combined_padded_sequence = pad_sequences(combined_sequence, maxlen=max_len)
+
+    # 예측
+    y_pred = model.predict(combined_padded_sequence)
+
+    y_pred_result = encoder.inverse_transform(np.argmax(y_pred, axis=1))
     # 예측 값 디코딩 
-    pred = label_encoders["key2"].inverse_transform(pred)
-    return render_template('after.html', datas=datas, pred=pred[0])
+    # pred = label_encoders["key2"].inverse_transform(pred)
+    return render_template('after.html', datas=datas, pred=y_pred_result[0])
 
 
 if __name__ == '__main__':
--- DA/database/to_db.py
+++ DA/database/to_db.py
@@ -1,5 +1,5 @@
 import pandas as pd
-df = pd.read_csv('../data/raw_postpro.csv', encoding='cp949')
+df = pd.read_csv('../data/raw_postpro.csv')
 df["견적화폐"] = df["견적화폐"].astype(str).str.strip()
 df["견적단가"] = df["견적단가"].astype(str).str.replace(",", "")
 df["견적단가"] = df["견적단가"].astype(int)
@@ -7,13 +7,15 @@ df["청구품목"] = df["청구품목"].str.strip()
 df["청구품목"] = df["청구품목"].str.replace('"', '')
 df["청구품목"] = df["청구품목"].str.replace("'", "")
 
-df_product = df[["Assembly" ,"key2", "발주처", "견적화폐","청구품목", '리드타임',"Machinery" ,"Part No.1", "견적단가", '출고운반선','Subject']].copy()
+df_product = df[["Assembly" ,"key2", "발주처", "견적화폐","청구품목", '리드타임',"Machinery" ,"Part No.1","Part No.2", "견적단가", '출고운반선','Subject','발주']].copy()
 df_product['id'] = df_product.index
 # 여러 컬럼명 변경
 df_product.rename(columns={'청구품목': 'item',
                            '발주처': 'company',
                            "견적화폐": "currency",
-                           'Part No.1': 'partNo1',
+                           '발주' : "date",
+                           'Part No.1': 'part_No1',
+                            'Part No.2' :'part_No2',
                            "리드타임" : "leadtime",
                            "key2" : "category",
                            "견적단가": "price",
@@ -24,7 +26,7 @@ df_product = df_product.set_index('id').reset_index()
 from sqlalchemy import create_engine
 
 # # MySQL 서버에 연결
-engine = create_engine('mysql+mysqlconnector://root:1234@localhost:3306/project')
+engine = create_engine('mysql+mysqlconnector://root:1234@localhost:3306/ship')
 #
 # # 데이터프레임을 MySQL 데이터베이스에 저장
-df_product.to_sql(name='product', con=engine, if_exists='replace', index=False)
+df_product.to_sql(name='item', con=engine, if_exists='append', index=False)
